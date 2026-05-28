5306c9db49d749520ac546344151cd0b0c3eabf1
shivamjxin <shivamjainz101@gmail.com>
2026-05-28 12:23:35 +0530

    feat: implement JSONL data logging for CV stream

--- integration_hub/server.py
+++ integration_hub/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -10,9 +11,14 @@ socketio = SocketIO(app, cors_allowed_origins="*")  # !!!CHANGE LATER!!! '*' all
 def hello_world():
     return "hello!"
 
+cv_log_file = open("../data_logs/raw_cv_stream.jsonl","a")
+
 @socketio.on('cv_frame')  # keeps the function on idle but the moment data is transmitted from camera_engine the function gets data shoved into it
 def receive_cv_data(data):
-    pass
+    cv_stream = json.dumps(data)
+    cv_log_file.write(cv_stream + "\n")
+    cv_log_file.flush()
+
 
 if __name__ == '__main__':
     socketio.run(app, port = 5000)
